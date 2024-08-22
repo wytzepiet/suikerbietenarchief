abbbb3fdf4a3106b3b6866cb24345419daa2d503
wytzepiet <wytzepiet@gmail.com>
2024-08-22 19:47:44 +0200

    added back assemblyai

---
+++ src/libs/services/assemblyai.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { AssemblyAI } from "assemblyai";
+
+const client = new AssemblyAI({ apiKey: process.env.ASSEMBLYAI_API_KEY! });
+
+export function transcribe(playbackId: string) {
+  return client.transcripts.submit({
+    audio_url: `https://stream.mux.com/${playbackId}/audio.m4a`,
+    language_code: "nl",
+    webhook_url: `https://shortly-fit-leopard.ngrok-free.app/api/webhooks/assemblyai?playback_id=${playbackId}`,
+  });
+}
+
+export function getTranscript(transcriptId: string) {
+  return client.transcripts.get(transcriptId);
+}
+
+export function deleteTranscript(transcriptId: string) {
+  return client.transcripts.delete(transcriptId);
+}
