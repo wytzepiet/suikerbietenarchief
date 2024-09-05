f2315926b35c0101c413450b9c604314be199104
wytzepiet <wytzepiet@gmail.com>
2024-09-05 12:18:13 +0200

    things

--- src/libs/services/supabase/server.ts
+++ src/libs/services/supabase/server.ts
@@ -1,10 +1,10 @@
-"user server";
-
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./types";
 
-export const supabase = () =>
+export function supabase() {
+  "use server";
   createClient<Database>(
     process.env.VITE_SUPABASE_URL ?? "",
     process.env.SUPABASE_SERVICE_ROLE_KEY ?? ""
   );
+}
